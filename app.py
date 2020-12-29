331a0b09f468a577c3169ce7ce395d3ffb1fa251
Tigran Asriyan <tigran.asriyan@gmail.com>
2020-12-29 12:43:43 -0500

    add user model

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ from resources.question import Question
 from resources.answer import Answer
 
 app = Flask(__name__)
-app.config['SQALCHEMY_DATABASE_URI'] = os.environ.get(
-    'DATABASE_URL', 'sqlite:///data.db')
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+# app.config['SQALCHEMY_DATABASE_URI'] = os.environ.get(
+#     'DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
 api.add_resource(Exam, '/exam/<string:gid>')
@@ -21,23 +21,25 @@ api.add_resource(ExamList, '/exams')
 # api.add_resource(Question, '/question/<string:gid>')
 # api.add_resource(Answer, '/answer/<string:gid>')
 
-if __name__ == '__main__':
-    from db import db
-    db.init_app(app)
+# if __name__ == '__main__':
+#     from db import db
+#     db.init_app(app)
 
-    if app.config['DEBUG']:
-        @app.before_first_request
-        def create_tables():
-            db.create_all()
+#     if app.config['DEBUG']:
+#         @app.before_first_request
+#         def create_tables():
+#             db.create_all()
 
-    app.run(port=5000)
+#     app.run(port=5000)
 
 # Development Env
-# @app.before_first_request
-# def create_tables():
-#     db.init_app(app)
-#     db.create_all()
 
 
-# if __name__ == '__main__':
-#     app.run(debug=True)  # important to mention debug=True
+@app.before_first_request
+def create_tables():
+    db.init_app(app)
+    db.create_all()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)  # important to mention debug=True
